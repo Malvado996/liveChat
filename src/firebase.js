edcfffa3d0109911ccf233ec6af30ebc4e6ac88e
Malvado996 <malvado996@gmail.com>
2022-05-23 09:49:39 -0400

    commit

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAT32sFduFMe1xBFWjxKj7TsM1cHOrxAkc",
+//   authDomain: "rowdymoto-db7c1.firebaseapp.com",
+//   projectId: "rowdymoto-db7c1",
+//   storageBucket: "rowdymoto-db7c1.appspot.com",
+//   messagingSenderId: "995766182368",
+//   appId: "1:995766182368:web:0ef8476d52a0d6be0e0b85",
+//   measurementId: "G-FCW2SV291F"
+// };
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
